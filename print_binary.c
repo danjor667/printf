ea4b2c582fdcd37c3e1bb080a8d202c391781de4
danjor667 <jordannguepi@gmail.com>
2023-04-24 00:44:35 -0700

    adding the print_binary func

--- print_binary.c
+++ print_binary.c
@@ -1,4 +1,26 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdlib.h>
+/**
+ * print_binary - a function to print the binary representatio
+ * of a deciml number
+ * @i: decimal input
+ * Return: number of digits of binary
+ */
 int print_binary(unsigned int i)
+{
+	unsigned int count = 0;
+
+	if (i < 2)
+	{
+		_putchar('0' + i);
+		count++;
+	}
+	else
+	{
+		count += print_binary(i / 2);
+		_putchar('0' + i % 2);
+		count++;
+	}
+	return (count);
+}
