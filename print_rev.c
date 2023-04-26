4d75b74920b6ad5cd4b33e6bf6423684f2d017af
katiamiyu <katiamiyu@gmail.com>
2023-04-26 14:23:14 +0000

    includes reverse string function

---
+++ print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_rev - reverse inputed string parameter value
+ * @s: input string parameter(array char)
+ * Return: integer representing length
+ */
+int print_rev(char *s)
+{
+	int x = 0, count = 0;
+
+	if (s != NULL)
+	{
+		while (*s != '\0')
+		{
+			s++;
+			x += 1;
+		}
+		s--;
+		while (x > 0)
+		{
+			_putchar(*s);
+			s--;
+			x -= 1;
+			count += 1;
+		}
+		return (count);
+	}
+	s = "(null)";
+	_puts(s);
+	return (_strlen(s));
+}
